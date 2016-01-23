f08422707e5956ff1b23dbb32a43a6efe373fb26
Branden Soropia <b.soropia@gmail.com>
2016-01-23 05:06:53 -0500

    Implemented navigation from Main Activity to Check Consciousness Activity, but not tested.

---
+++ HealthApp/app/src/main/java/uofthacks/healthapp/CheckConsciousness.java
@@ -0,0 +1,21 @@
+package uofthacks.healthapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+public class CheckConsciousness extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.check_consciousness_activity);
+    }
+
+    public void sendToNextStep(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, EmergencyContact.class);
+        startActivity(intent);
+    }
+}
---
+++ HealthApp/app/src/main/java/uofthacks/healthapp/EmergencyContact.java
@@ -0,0 +1,13 @@
+package uofthacks.healthapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmergencyContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_emergency_services_activity);
+    }
+}
