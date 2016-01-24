8bc71ca9140e4c7b59de047bb9f4158aa674895e
Branden Soropia <b.soropia@gmail.com>
2016-01-23 19:06:13 -0500

    Created Perform CPR activity, added some buttons and stuff in general.

--- HealthApp/app/src/main/java/uofthacks/healthapp/ApplyBand.java
+++ HealthApp/app/src/main/java/uofthacks/healthapp/ApplyBand.java
@@ -2,6 +2,7 @@ package uofthacks.healthapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ApplyBand extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class ApplyBand extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apply_band);
+    }
+
+    public void evaluateSituation(View view) {
+        // Take band info and evaluate what to do next
+        // Move to CPR
+        // Move to HAINES recovery position
+        // Conduct secodnary assesment
     }
 }
--- HealthApp/app/src/main/java/uofthacks/healthapp/EmergencyContact.java
+++ HealthApp/app/src/main/java/uofthacks/healthapp/EmergencyContact.java
@@ -2,6 +2,9 @@ package uofthacks.healthapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.net.Uri;
 
 public class EmergencyContact extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class EmergencyContact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact_emergency_services_activity);
+    }
+
+    public void callEmergencyPersonnel(View view) {
+        String number = "6478343317";
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" +number));
+        startActivity(intent);
     }
 }
